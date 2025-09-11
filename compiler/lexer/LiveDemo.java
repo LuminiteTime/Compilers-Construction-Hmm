5a56104b6a208478992743108e2c39306a0b60cb
Mikhail <trifonov2812@gmail.com>
2025-09-11 17:22:19 +0300

    small fixes

--- compiler/lexer/LiveDemo.java
+++ compiler/lexer/LiveDemo.java
@@ -10,7 +10,7 @@ public class LiveDemo {
         "// Comment demo\nvar y: real;"
     };
 
-1    public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
